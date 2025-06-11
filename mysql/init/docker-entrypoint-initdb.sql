6097a4da915b552fb1f9a193cabb992279a886a9
Vinit Chauhan <vinit.chauhan@cgi.com>
2025-06-11 10:17:59 -0400

    Fix certificaete issue for Windows WSL env

--- mysql/init/docker-entrypoint-initdb.sql
+++ mysql/init/docker-entrypoint-initdb.sql
@@ -14,12 +14,4 @@ INSERT INTO students (name, email, age) VALUES
 ('Sophia Davis', 'sophia.d@email.com', 21),
 ('William Wilson', 'william.w@email.com', 23),
 ('Olivia Martinez', 'olivia.m@email.com', 20),
-('James Taylor', 'james.t@email.com', 22),
-('Ava Anderson', 'ava.a@email.com', 19),
-('Benjamin Thomas', 'benjamin.t@email.com', 21),
-('Isabella Jackson', 'isabella.j@email.com', 20),
-('Lucas White', 'lucas.w@email.com', 22),
-('Mia Harris', 'mia.h@email.com', 19),
-('Ethan Martin', 'ethan.m@email.com', 21),
-('Charlotte Thompson', 'charlotte.t@email.com', 20),
 ('Alexander Garcia', 'alexander.g@email.com', 23);
